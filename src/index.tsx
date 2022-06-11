6dd1975f0a0b3721820f2f7d0d5fe9a1b59f12ef
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-11 18:11:46 +0900

    chore: styled-components 설정 및 react-query 베스트케이스를 추가한다

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
+import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+import normalize from "styled-normalize";
+
 import App from "@/App";
 
 const container = document.getElementById("root");
@@ -7,4 +12,27 @@ if (!container) throw new Error("Failed to find the root element");
 
 const root = createRoot(container);
 
-root.render(<App />);
+export const theme = {
+  color: {},
+};
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const queryClient = new QueryClient();
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Suspense fallback={<div>로딩중...</div>}>
+        <App />
+      </Suspense>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
