0b55c1a31491121dc0b996199e867629032562ea
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-11 15:41:18 +0900

    chore: webpack 설정을 추가한다

---
+++ webpack/webpack.base.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
+const DIST_PATH = path.resolve(PROJECT_ROOT, 'dist');
+const SRC_PATH = path.resolve(PROJECT_ROOT, 'src');
+
+module.exports = {
+  entry: path.resolve(SRC_PATH, 'index.tsx'),
+  output: {
+    path: DIST_PATH,
+    filename: '[name].js',
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@': SRC_PATH,
+    },
+    modules: ['node_modules'],
+  },
+  plugins: [new Dotenv()],
+};
