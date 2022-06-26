d150ab80c090237919dbd9b71a13013184aa6f25
SongWon(Chris) Lee <songwon.lee@smartfoodnet.com>
2022-06-26 20:20:22 +0900

    chore: 불필요한 예시용 컴포넌트 제거

--- src/pages/Test/Test.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Test from "./Test";
-
-export default {
-  title: "components/test",
-  component: Test,
-} as ComponentMeta<typeof Test>;
-
-export const Template: ComponentStory<typeof Test> = () => <Test />;
--- src/pages/Test/Test.tsx
+++
@@ -1,7 +0,0 @@
-import * as S from "./Test.styled";
-
-const Test = () => {
-  return <S.Container>Test</S.Container>;
-};
-
-export default Test;
