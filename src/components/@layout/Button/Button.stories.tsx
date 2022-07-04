052a294204ba211670f2cc99edfb944ebf7ce88b
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-07-04 23:14:23 +0900

    [#23] 공통스타일 파일분리 + 버튼, 탭 초안 작성
    <공통스타일 파일분리>
    * 스타일-테마 모음 : src/styles/theme.ts
    * 스타일-타입정의 모음 : src/styles/styled.d.ts
    * 전역-스타일 모음 : src/styles/global-style.ts

---
+++ src/components/@layout/Button/Button.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, Story } from "@storybook/react";
+import { useState } from "react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    // label: { type: "text" },
+    
+  },
+} as Meta;
+
+
+// 기본 포맷을 정해두고 bind로 제어 
+const Template: Story<ButtonProps> = (args) => {
+  const [isActive, setActive] = useState(false);
+
+  return (
+    <Button {...args} />
+  );
+};
+
+// onClick={() => setActive((isActive) => !isActive)} 
+// export const Basic = Template.bind({});
+
+
+//const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+// 각각이 새로운 스토리들
+// export const Small = () => <Button size="small" label="button" />; 얘와 같음
+export const Small = Template.bind({});
+Small.args = {
+  size: 'sm',
+  bgColor: 'black',
+  label: '작은버튼',
+  shadow : true,
+};
+
+export const Middle = Template.bind({});
+Middle.args = {
+  size: 'md',
+  bgColor: 'blue',
+  label: '중간버튼',
+  shadow : true,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'lg',
+  bgColor: 'white',
+  label: '큰버튼',
+  shadow : true,
+};
