59c8edde6169fe48db3d222d39f361cd5e70433c
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-07-04 23:14:23 +0900

    [#23] 공통스타일 파일분리 + 버튼, 탭 초안 작성
    <공통스타일 파일분리>
    * 스타일-테마 모음 : src/styles/theme.ts
    * 스타일-타입정의 모음 : src/styles/styled.d.ts
    * 전역-스타일 모음 : src/styles/global-style.ts

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,189 @@
+// global-style.ts
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+	${reset}
+	
+	* {
+		margin:0;
+		padding:0;
+		-webkit-text-size-adjust:none;
+		word-break:break-all;
+		box-sizing:border-box;
+	&,
+	&:before,
+	&:after {
+			-webkit-box-sizing:border-box;
+			box-sizing: border-box;
+		}
+	}
+
+	html {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		overflow-y:scroll;
+	}
+
+	body {
+		-webkit-text-size-adjust: none;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	body, 
+	div, 
+	dl, 
+	dt, 
+	dd, 
+	ul, 
+	ol, 
+	li, 
+	h1, 
+	h2, 
+	h3, 
+	h4, 
+	h5, 
+	h6, 
+	pre, 
+	code, 
+	form, 
+	fieldset, 
+	legend,
+	p, 
+	blockquote, 
+	table, 
+	th, 
+	td, 
+	input, 
+	select, 
+	textarea, 
+	button {
+		margin:0;
+		padding:0;
+		word-break:break-all;
+	}
+
+	body, input, textarea, select, button, table {
+		font-family: AppleGothicNeoSD, 'Apple SD Gothic Neo', 맑은고딕, 'Malgun Gothic', arial, sans-serif;
+		font-size:13px; 
+		font-weight:400; 
+		line-height:18px;
+		color:#606060;
+	}
+
+	i, 
+	em {
+		font-style:normal
+	}
+
+	fieldset, 
+	img {
+		border:0 none
+	}
+	
+	img  {
+		vertical-align:middle
+	}
+
+	video {
+		vertical-align:middle
+	}
+
+	button {
+		border:0 none; 
+		border-radius:0; 
+		background-color:transparent; 
+		cursor:pointer;
+		-webkit-appearance: none;
+		outline: none;
+	}
+	
+	a {
+		color:inherit; 
+		text-decoration:none;
+
+		&:active {
+				outline: none;
+				color: inherit;
+		}
+	}
+	
+	article, 
+	aside, 
+	details, 
+	figcaption, 
+	figure, 
+	footer, 
+	header, 
+	hgroup, 
+	menu, 
+	nav, 
+	section {
+		display:block;
+		margin:0;
+		padding:0;
+	}
+
+	strong {
+		font-weight:inherit;
+	}
+
+	.blind {
+		display:inline-block;
+		position:absolute;
+		z-index:-10;
+		width:0 !important;
+		height:0 !important;
+		overflow:hidden !important;
+		font-size:medium;
+		line-height:normal;
+		word-break:break-all
+	}
+
+	input, 
+	select, 
+	textarea, 
+	button {
+		vertical-align:middle;
+		border:none;
+	}
+
+	label {
+		vertical-align:middle;
+	}
+	textarea {
+		width:100%; 
+		border:0; 
+		overflow-y:auto; 
+		resize:none
+	}
+
+	h {
+
+		&1,
+		&2,
+		&3,
+		&4,
+		&5,
+		&6 {
+				margin: 0;
+				padding: 0;
+				font-weight: normal;
+		}
+	}
+
+	ul,
+	ol,
+	dl {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+`;
