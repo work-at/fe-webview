e6776a7b9f593797afae6cdf40fa26c95694323e
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-07-04 23:14:23 +0900

    [#23] 공통스타일 파일분리 + 버튼, 탭 초안 작성
    <공통스타일 파일분리>
    * 스타일-테마 모음 : src/styles/theme.ts
    * 스타일-타입정의 모음 : src/styles/styled.d.ts
    * 전역-스타일 모음 : src/styles/global-style.ts

---
+++ src/components/@layout/Tabs/Tabs.tsx
@@ -0,0 +1,48 @@
+import { useState, useCallback, memo } from "react";
+import * as S from "./Tabs.styled";
+
+export type TabItem = {
+  id: string;
+  label: string;
+};
+
+export type TabsProps = {
+  items: TabItem[];
+  /** 탭 아이템이 선택되었을 때 동작하는 핸들러 */
+  onSelect: (id: string) => void;
+};
+
+const Tabs = ({ items, onSelect }: TabsProps) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+
+  const handleTabItemSelect = useCallback(
+    (id: string) => {
+      const newSelectedItemIndex = items.findIndex((item) => item.id === id);
+
+      if (newSelectedItemIndex === undefined) {
+        throw Error("선택된 탭 아이템을 찾을 수 없습니다.");
+      }
+
+      setSelectedItemIndex(newSelectedItemIndex);
+      onSelect(id);
+      console.log(newSelectedItemIndex);
+    },
+    [items]
+  );
+
+  return (
+    <S.TabWrap>
+      <S.TabList role="tablist">
+        {items.map((item) => (
+          <S.TabItem itemCount={items.length} selectedItemIndex={selectedItemIndex}>
+            <S.TabLink onClick={() => {handleTabItemSelect(item.id)}}>
+              <S.TabText>{item.label}</S.TabText>
+            </S.TabLink>
+          </S.TabItem>
+        ))}
+      </S.TabList>
+    </S.TabWrap>
+  );
+};
+
+export default memo(Tabs);
