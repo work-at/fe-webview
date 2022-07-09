8283c63df8fd378772db3702dd3e5352686f7578
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-07-10 04:53:05 +0900

    [#27] 아이콘 컴포넌트 작업 1차 완료

---
+++ src/assets/Icon.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from 'react';
+import * as icons from '@/assets/svg';
+
+export type IconType = keyof typeof icons;
+export const iconTypes: IconType[] = Object.keys(icons) as IconType[];
+
+export type IconProps = {
+  icon: IconType;
+  size?: string | number;
+};
+
+export const Icon = ({ icon, size }: IconProps):ReactElement => {
+  const SVGIcon = icons[icon];
+  const widthPx =
+    size &&
+    (typeof size === 'number' ? `${size}px` : `${size.replace('px', '')}px`);
+  return (
+    <SVGIcon
+      style={{
+        width: widthPx,
+        height: 'auto',
+      }}
+    />
+  );
+};
