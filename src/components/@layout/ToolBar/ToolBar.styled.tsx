64a10b8ba8409c9c132ce61cddfdcd525d2c5e14
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-07-10 04:43:18 +0900

    [#23] Tag, Toolbar, Card 컴포넌트 퍼블리싱 / style폴더 경로변경

---
+++ src/components/@layout/ToolBar/ToolBar.styled.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { theme } from "@/assets/styles/theme";
+
+export const ToolBar = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: ${({ theme }) => theme.colors.white};
+  height: 83px;
+  z-index: 99;
+`;
+
+export const MenuList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+`;
+
+export const ListItem = styled.li`
+  width: calc(100% / 4);
+`;
+
+export const ListButton = styled.button`
+  display: block;
+  width: 100%;
+`;
+
+export const MenuIcon = styled.i`
+  display: block;
+  width: 30px;
+  height: 30px;
+  margin: 0 auto 6px;
+`;
+
+export const MenuText = styled.span`
+  color: #606161;
+  font-size: 12px;
+  line-height: 14px;
+`;
