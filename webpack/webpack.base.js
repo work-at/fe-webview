70873d054fd5cae1f0e11a08ec9a33a3e9207cc9
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-07-19 01:16:30 +0900

    chore: stackflow, css-loader, patch-package 모듈을 설치한다

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -40,6 +40,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
   resolve: {
