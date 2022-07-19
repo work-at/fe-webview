6f4e767a800056ffb3bccb260301c4177f673e7a
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-07-20 01:03:52 +0900

    feat: StackLayout와 패치파일 추가 및 path에 stack 필드를 추가한다

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -2,52 +2,64 @@ export const PATH = {
   full: "/",
   fragments: "/",
   LOGIN: {
+    stack: "LoginPage",
     full: "/login",
     fragments: "login",
   },
   SIGN_UP: {
+    stack: "SignUpPage",
     full: "/sign-up",
     fragments: "sign-up",
   },
   ACCOMMODATION: {
+    stack: "AccommodationPage",
     full: "/accommodation",
     fragments: "accommodation",
   },
   MAP: {
+    stack: "MapPage",
     full: "/map",
     fragments: "map",
   },
   CAFE: {
+    stack: "CafeDetailPage",
     full: "/cafe",
     fragments: "cafe",
 
     CAFE_REVIEW: {
+      stack: "CafeDetailPage",
       full: "/cafe/review",
       fragments: "review",
     },
   },
   DINER: {
+    stack: "DinerDetailPage",
     full: "/diner",
     fragments: "diner",
 
     DINER_REVIEW: {
+      stack: "DinerReviewPage",
       full: "/diner/review",
       fragments: "review",
     },
   },
   WORKER: {
+    stack: "WorkerDetailPage",
     full: "/worker",
     fragments: "worker",
   },
   WORK_CHAT: {
+    stack: "WorkChatPage",
     full: "/work-chat",
     fragments: "work-chat",
   },
   COMMUNITY: {
+    stack: "CommunityPage",
     full: "/community",
     fragments: "community",
   },
   MY_PAGE: {
+    stack: "MyPage",
     full: "/my-page",
     fragments: "my-page",
   },
