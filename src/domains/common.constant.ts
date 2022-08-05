c411de3138c09d20983301d8f22255ed5805b60a
SongWon(Chris) Lee <songwon.lee@smartfoodnet.com>
2022-08-06 00:22:56 +0900

    feat: common type, constant, text mapper 추가

---
+++ src/domains/common.constant.ts
@@ -0,0 +1,22 @@
+export const JOBS = [
+  "development",
+  "designer",
+  "PM",
+  "Marketing",
+  "freelancer",
+  "profession",
+  "office-worker",
+] as const;
+
+export const YEAR_OF_SERVICES = ["1~2", "3~4", "5~6", "7~8", "9~10", "11+"] as const;
+
+export const DESIRED_ACTIVITIES = [
+  "junior-gather",
+  "senior-gather",
+  "work-together",
+  "passionate",
+  "job-talk",
+  "career-concern",
+  "diner-mate",
+  "hang-around-after",
+] as const;
