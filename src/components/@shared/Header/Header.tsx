c758785a5026f8f1accee9f7d8fec42a43871757
EUNSUN JUNG <108320337+alice-eunsun@users.noreply.github.com>
2022-08-17 13:46:01 +0900

    [#93] 숙소 메인 페이지 퍼블리싱

--- src/components/@shared/Header/Header.tsx
+++ src/components/@shared/Header/Header.tsx
@@ -44,7 +44,8 @@ const Header = ({
 
       <S.HeaderInner useBack={useBack}>
         {useBack && (
-          <S.BtnBack bgColor={bgColor} onClick={() => pop()}>
+          // <S.BtnBack bgColor={bgColor} onClick={() => pop()}>
+          <S.BtnBack bgColor={bgColor}>
             <Icon icon="BtnBack" size={24} />
           </S.BtnBack>
         )}
