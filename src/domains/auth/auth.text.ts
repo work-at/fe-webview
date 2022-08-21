209e7bca35f1f10648290e4efe34b26c25995fc9
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-08-22 04:52:53 +0900

    chore: 서버와 position, working year 데이터를 맞춘다

--- src/domains/auth/auth.text.ts
+++ src/domains/auth/auth.text.ts
@@ -1,20 +1,22 @@
 import { KeyOf, ValueOf } from "type-util";
 
 export const POSITION = {
-  IT_ENGINEER: "개발",
+  ENGINEER: "개발",
   DESIGNER: "디자이너",
-  IT_PRODUCT_MANAGER: "기획",
+  PRODUCT: "기획 / PM",
   MARKETING: "마케팅",
-  OFFICE_WORKER: "사무직",
-  PROFESSIONAL: "전문직",
-  FREELANCER: "프리랜서",
+  OFFICE: "사무직",
+  EXPERT: "전문직",
+  FREE: "프리랜서",
 } as const;
 
 export const WORKING_YEAR = {
-  JUNIOR: "1~4년(주니어)",
-  MID_LEVEL: "5~8년(미드레벨",
-  SENIOR: "9~12년(시니어)",
-  OVER: "12년 이상",
+  NEW_STAFF: "1~2년차",
+  JUNIOR: "3~4년차",
+  MID_LEVEL: "5~6년차",
+  SENIOR: "7~8년차",
+  EXPERT: "9~10년차",
+  OVER: "11년차~",
 } as const;
 
 export type PositionType = KeyOf<typeof POSITION>;
