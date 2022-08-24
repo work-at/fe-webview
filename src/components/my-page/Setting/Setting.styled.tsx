8eb45f19889e77d27d69833b9590b1c98db1fdf0
SongWon(Chris) Lee <songwon.lee@smartfoodnet.com>
2022-08-25 03:22:48 +0900

    feat: 설정 기능 개발 및 퍼블리싱 적용

--- src/components/my-page/Setting/Setting.styled.tsx
+++ src/components/my-page/Setting/Setting.styled.tsx
@@ -1,81 +1,68 @@
 import styled from "styled-components";
 import { theme } from "@/assets/styles/theme";
-import BaseButton from "@/components/@shared/Button/Button";
 
-export const MyInfoEditWrap = styled.form`
-  position: relative;
-  padding-bottom: 115px;
-`;
+export const MySettingWrap = styled.div``;
 
-export const MyInfoItem = styled.dl`
+export const SettingList = styled.ul``;
+
+export const SettingItem = styled.li`
   display: flex;
-  align-items: center;
-  padding: 22px 7.4667vw;
-  border-bottom: 1px solid ${theme.colors.gray08};
-  width: 100%;
-  &:last-child {
-    padding-bottom: 0;
-    border: none;
-  }
-`;
-
-export const MyInfoFullItem = styled(MyInfoItem)`
-  flex-wrap: wrap;
-`;
-
-export const ItemHead = styled.dt`
-  ${theme.fonts.Bold04};
-  color: ${theme.colors.black};
-  padding-right: 36px;
-  flex-shrink: 0;
-`;
-
-export const HeadEtcTxt = styled.span`
-  ${theme.fonts.Medium04};
-  color: ${theme.colors.gray04};
-  padding-left: 2px;
-  display: inline-block;
-  vertical-align: middle;
-`;
-
-export const ItemBody = styled.dd`
-  display: flex;
-  align-items: center;
   justify-content: space-between;
-  ${theme.fonts.Medium02};
-  color: ${theme.colors.gray03};
-  flex-shrink: 1;
-  width: 100%;
-  > button {
-    float: right;
+  ${theme.fonts.Bold04};
+  color: ${theme.colors.gray01};
+  border-top: 1px solid ${theme.colors.gray08};
+  padding: 30px 7.4667vw;
+  &:first-child {
+    border-top: none;
   }
 `;
 
-export const BtnEdit = styled.button`
-  margin-left: auto;
+export const ToggleWrapper = styled.div`
+  position: relative;
 `;
 
-export const TxtWrap = styled.div`
-  width: 100%;
-
-  ${theme.fonts.Medium03};
-  background: ${theme.colors.gray10};
-  padding: 14px;
-  margin-top: 20px;
-  border-radius: 10px;
-  outline: none;
+export const ToggleBoxLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50px;
+  height: 26px;
+  border-radius: 13px;
+  background: ${theme.colors.gray07};
+  cursor: pointer;
+  &:after {
+    content: "";
+    display: block;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    margin: 3px;
+    background: ${theme.colors.white};
+    transition: 0.2s;
+  }
 `;
 
-export const Txt = styled.span``;
-
-export const ChekBoxWrap = styled.div`
-  margin-top: 18px;
+export const ToggleBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 13px;
+  width: 50px;
+  height: 26px;
+  &:checked + ${ToggleBoxLabel} {
+    background: ${theme.colors.mainColor};
+    &:after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      width: 20px;
+      height: 20px;
+      margin-left: 26px;
+      transition: 0.2s;
+    }
+  }
 `;
 
-export const Input = styled.input`
-  outline: none;
-`;
-
-export const Button = styled(BaseButton)`
-  min-width: 75px;
+export const BtnLogout = styled.button`
+  ${theme.fonts.Bold04};
+  color: ${theme.colors.gray01};
 `;
