df072f70f2bf1a697c32169bfc25f8c80f551d3a
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-09-02 20:14:16 +0900

    fix: stackflow patch 후 고쳐야 할 부분을 고친다

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -3,10 +3,12 @@ import StackLayout from "@/components/@layout/StackLayout/StackLayout";
 import useCoordinates from "@/services/useCoordinates/useCoordinates";
 import { useInjectKakaoMapApi } from "react-kakao-maps-sdk";
 import MapManager from "@/components/map/MapManager";
+import { useStack } from "@stackflow/react";
 
 const API_KEY = process.env.KAKAO_MAP_API_KEY;
 
 const MapPage = () => {
+  const stack = useStack();
   const { userCoordinates } = useCoordinates();
 
   const { data: userAddress } = useUserAddressQuery(
@@ -45,6 +47,14 @@ const MapPage = () => {
     return (
       <StackLayout appBar={{ title: userAddress }} navigationPath="map">
         <div>지도 정보를 불러오는데 실패하였습니다</div>
+      </StackLayout>
+    );
+  }
+
+  if (stack.activities.filter(({ transitionState }) => transitionState === "enter-done").length > 1) {
+    return (
+      <StackLayout isHide navigationPath="map">
+        <MapManager userCoordinates={userCoordinates} />
       </StackLayout>
     );
   }
