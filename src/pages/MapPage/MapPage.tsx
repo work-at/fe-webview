e369d6c46e19ce0f568787201f84f676bfdb3361
SongWon(Chris) Lee <songwon.lee@smartfoodnet.com>
2022-09-02 17:39:58 +0900

    feat: 지도 로딩 및 에러 UI 분기처리 수정

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,10 +1,8 @@
-import * as S from "./MapPage.styled";
 import { useUserAddressQuery } from "@/domains/user";
 import StackLayout from "@/components/@layout/StackLayout/StackLayout";
 import useCoordinates from "@/services/useCoordinates/useCoordinates";
 import { useInjectKakaoMapApi } from "react-kakao-maps-sdk";
 import MapManager from "@/components/map/MapManager";
-import { createPortal } from "react-dom";
 
 const API_KEY = process.env.KAKAO_MAP_API_KEY;
 
@@ -28,15 +26,27 @@ const MapPage = () => {
   });
 
   if (!userCoordinates) {
-    return <div>현재 좌표 로딩중</div>;
+    return (
+      <StackLayout appBar={{ title: userAddress }} navigationPath="map">
+        <div>현재 좌표 로딩중</div>
+      </StackLayout>
+    );
   }
 
   if (isMapLoading) {
-    return <div>지도 로딩중</div>;
+    return (
+      <StackLayout appBar={{ title: userAddress }} navigationPath="map">
+        <div>지도 로딩중</div>{" "}
+      </StackLayout>
+    );
   }
 
   if (mapLoadingError) {
-    return <div>지도 정보를 불러오는데 실패하였습니다</div>;
+    return (
+      <StackLayout appBar={{ title: userAddress }} navigationPath="map">
+        <div>지도 정보를 불러오는데 실패하였습니다</div>
+      </StackLayout>
+    );
   }
 
   return (
