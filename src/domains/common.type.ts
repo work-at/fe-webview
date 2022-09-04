810bcd92d2890429f306bb65baee2047c2dfbdea
SongWon(Chris) Lee <songwon.lee@smartfoodnet.com>
2022-09-04 21:37:12 +0900

    feat: 문의하기 API 연동

--- src/domains/common.type.ts
+++ src/domains/common.type.ts
@@ -1,4 +1,4 @@
-import { DESIRED_ACTIVITIES, JOBS, YEAR_OF_SERVICES } from "./common.constant";
+import { DESIRED_ACTIVITIES, JOBS, REPORTS, YEAR_OF_SERVICES } from "./common.constant";
 
 // TODO : enum 으로 만들기
 export type DesiredActivity = typeof DESIRED_ACTIVITIES[number]["name"];
@@ -6,3 +6,5 @@ export type DesiredActivity = typeof DESIRED_ACTIVITIES[number]["name"];
 export type Job = typeof JOBS[number]["name"];
 
 export type YearOfService = typeof YEAR_OF_SERVICES[number]["name"];
+
+export type Report = typeof REPORTS[number];
