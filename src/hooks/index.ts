da2059c28a5d4812421d2a98402032819c5727cd
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-09-10 12:38:23 +0900

    feat: useInterval 커스텀 훅을 추가한다

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { default as useDebounce } from "./useDebounce";
 export { default as useDetectOutsideClick } from "./useDetectOutsideClick";
 export { default as useIntersectionObserver } from "./useIntersectionObserver";
+export { default as useInterval } from "./useInterval";
 export { default as usePageVisibility } from "./usePageVisibility";
 export { default as useScrollDrag } from "./useScrollDrag";
 
