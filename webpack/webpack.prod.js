9f4b50621e641d4e5416990892a083926889de04
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-09-14 14:51:52 +0900

    fix: wepack prod 설저에  @typescript-eslint/no-var-requires 룰을 off 한다

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
